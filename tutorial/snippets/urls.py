0a580b3c83dbb317f02c5542f8e80ed9a7b21fdf
Venkata Rama Sai Badam <vbadam@atrihub.io>
2025-04-15 10:55:35 -0700

    feat (Rest_Tutorial): Viewsets and Routers [ICTRAINING-1995]

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,31 +1,16 @@
 from django.urls import path, include
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+
 from snippets import views
 
-'''
-n Django, class-based views (CBVs) like SnippetList, SnippetDetail, UserList, 
-and UserDetail need to be converted into callable view functions that can handle HTTP requests. 
-The as_view() method does this conversion.
-'''
+# Create a router and register our ViewSets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet, basename='snippet')
+router.register(r'users', views.UserViewSet, basename='user')
 
-# API endpoints
-urlpatterns = format_suffix_patterns([
-    path('', views.api_root),
-    path('snippets/',
-        views.SnippetList.as_view(),
-        name='snippet-list'),
-    path('snippets/<int:pk>/',
-        views.SnippetDetail.as_view(),
-        name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/',
-        views.SnippetHighlight.as_view(),
-        name='snippet-highlight'),
-    path('users/',
-        views.UserList.as_view(),
-        name='user-list'),
-    path('users/<int:pk>/',
-        views.UserDetail.as_view(),
-        name='user-detail'),
-    path('api-auth/', 
-         include('rest_framework.urls')),
-])
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
+]
+
