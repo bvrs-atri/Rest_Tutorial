19b3b396934f68ced464a4fc6db7a1e3be015cea
Venkata Rama Sai Badam <vbadam@atrihub.io>
2025-04-15 10:37:22 -0700

    feat (Rest_Tutorial): Relationships & Hyperlinked APIs [ICTRAINING-1994]

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -8,14 +8,24 @@ and UserDetail need to be converted into callable view functions that can handle
 The as_view() method does this conversion.
 '''
 
-urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
-urlpatterns += [
-    path('api-auth/', include('rest_framework.urls')),
-]
+# API endpoints
+urlpatterns = format_suffix_patterns([
+    path('', views.api_root),
+    path('snippets/',
+        views.SnippetList.as_view(),
+        name='snippet-list'),
+    path('snippets/<int:pk>/',
+        views.SnippetDetail.as_view(),
+        name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/',
+        views.SnippetHighlight.as_view(),
+        name='snippet-highlight'),
+    path('users/',
+        views.UserList.as_view(),
+        name='user-list'),
+    path('users/<int:pk>/',
+        views.UserDetail.as_view(),
+        name='user-detail'),
+    path('api-auth/', 
+         include('rest_framework.urls')),
+])
